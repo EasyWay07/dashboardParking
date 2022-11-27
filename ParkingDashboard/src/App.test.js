e3e0abd832170d4457eddfc10443878bfa9a9afa
Alaa Bouassida <alaabouassida97@gmail.com>
2022-11-27 20:59:45 +0100

    update jenkins script

--- ParkingDashboard/src/App.test.js
+++ ParkingDashboard/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Loader from '../src/components/Loader';
 
+
 test('renders learn react link', () => {
     render(<Loader />);
 });
