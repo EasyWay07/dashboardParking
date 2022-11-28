d7fa395e32edffa399003222903069af2ee49b4c
Alaa Bouassida <alaabouassida97@gmail.com>
2022-11-28 10:11:15 +0100

    update jenkins script

--- ParkingDashboard/server/server.js
+++ ParkingDashboard/server/server.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 const app = express();
 
+// Serve static files....
 app.use(express.static(__dirname + '/build'));
 
+// Send all requests to index.html
 app.get('/*', function(req, res) {
     res.sendFile (path.join(__dirname + '/build/index.html'));
 });
