4ba22260e404de2887e2b8c5b5a5e14baad07e43
Alaa Bouassida <alaabouassida97@gmail.com>
2022-11-28 09:48:29 +0100

    update jenkins script

--- ParkingDashboard/src/__test__/App.test.js
+++ ParkingDashboard/src/__test__/App.test.js
@@ -3,3 +3,29 @@
 test('deux plus deux font quatre', () => {
     expect(2 + 2).toBe(4);
   });
+
+  test('deux plus cinq font sept', () => {
+    expect(2 + 5).toBe(7);
+  });
+
+
+  test('deux plus trois font cinq', () => {
+    expect(2 + 3).toBe(5);
+  });
+
+
+  test('deux plus zero font deux', () => {
+    expect(2 + 0).toBe(2);
+  });
+
+  test('deux plus quatre font six', () => {
+    expect(2 + 4).toBe(6);
+  });
+
+  test('deux plus un font trois', () => {
+    expect(2 + 1).toBe(3);
+  });
+
+  test('deux plus quatre font cinq', () => {
+    expect(2 + 4).toBe(6);
+  });
